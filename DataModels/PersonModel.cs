a1906ad9c4b112c1eaf09255b570d4f0c9db014a
Deberson de Paula <deberson.paula@xpi.com.br>
2020-10-11 10:52:13 -0300

    added data models

---
+++ DataModels/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DataModels
+{
+    public class PersonModel
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobArea { get; set; }
+        public string JobDescriptor { get; set; }
+        public string JobTitle { get; set; }
+        public string JobType { get; set; }
+    }
+}
