2ad2d507493e71092d409ccf747b3afa36febdbf
Deberson de Paula <deberson.paula@xpi.com.br>
2020-10-11 11:05:06 -0300

    added EF app

---
+++ WebApi.EFCore/Controllers/PersonContext.cs
@@ -0,0 +1,20 @@
+﻿using DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.EFCore.Controllers
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PersonModel> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonModel>().ToTable("TB_PERSON");
+            modelBuilder.Entity<PersonModel>().HasKey(lc => new { lc.UserId });
+        }
+    }
+}
---
+++ WebApi.EFCore/Controllers/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace WebApi.EFCore.Controllers
+{
+    [Route("api/person")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly PersonContext personContext;
+
+        public PersonController(PersonContext personContext)
+        {
+            this.personContext = personContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPerson([FromRoute] int id)
+        {
+            try
+            {
+                var user = await personContext.People.FirstOrDefaultAsync(m => m.UserId == id);
+
+                if (user != null)
+                {
+                    return Ok(user);
+                }
+
+                return NotFound();
+            }
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
