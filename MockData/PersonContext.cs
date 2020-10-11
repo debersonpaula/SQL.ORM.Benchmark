d68b23b9e085f7ad1616090f007ab4c8260685a9
Deberson de Paula <deberson.paula@xpi.com.br>
2020-10-11 10:45:32 -0300

    added mock data exec

---
+++ MockData/PersonContext.cs
@@ -0,0 +1,22 @@
+﻿using DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace MockData.Models
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<PersonModel> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=localhost,1600;Initial Catalog=DB_BENCHMARK;User ID=sa;Password=DBPass123;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonModel>().ToTable("TB_PERSON");
+            modelBuilder.Entity<PersonModel>().HasKey(lc => new { lc.UserId });
+        }
+    }
+}
---
+++ MockData/Program.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using DataModels;
+using MockData.Models;
+
+namespace MockData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new PersonContext())
+            {
+                var persons = FakePerson().Generate(5000);
+                db.People.AddRange(persons);
+                db.SaveChanges();
+            }
+        }
+
+        static Faker<PersonModel> FakePerson()
+        {
+            return new Faker<PersonModel>()
+                .RuleFor(u => u.FirstName, (f,u) => f.Name.FirstName())
+                .RuleFor(u => u.LastName, (f, u) => f.Name.LastName())
+                .RuleFor(u => u.JobArea, (f, u) => f.Name.JobArea())
+                .RuleFor(u => u.JobDescriptor, (f, u) => f.Name.JobDescriptor())
+                .RuleFor(u => u.JobTitle, (f, u) => f.Name.JobTitle())
+                .RuleFor(u => u.JobType, (f, u) => f.Name.JobType());
+        }
+    }
+
+
+}
